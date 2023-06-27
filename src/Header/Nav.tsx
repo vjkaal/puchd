b8dd00621b41afaf93359b471580c4bec87a60ec
Vanshaj Kaushal <vanshajkaushal@yahoo.com>
2023-06-27 13:00:42 +0530

    added link array for main nav header

--- src/Header/Nav.tsx
+++ src/Header/Nav.tsx
@@ -1,3 +1,44 @@
 export function Nav() {
-  
+  const links = [
+    {
+      class: 'home',
+      href: '#',
+      text: 'home'
+    },
+    {
+      class: 'about',
+      href: '#',
+      text: 'about'
+    },
+    {
+      class: 'academics',
+      href: '#',
+      text: 'academics'
+    },
+    {
+      class: 'administration',
+      href: '#',
+      text: 'administration'
+    },
+    {
+      class: 'research',
+      href: '#',
+      text: 'research'
+    },
+    {
+      class: 'admission',
+      href: '#',
+      text: 'admission'
+    },
+    {
+      class: 'campus',
+      href: '#',
+      text: 'campus'
+    },
+    {
+      class: 'contact',
+      href: '#',
+      text: 'contact'
+    },
+  ]
 }
