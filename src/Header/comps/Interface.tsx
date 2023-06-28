62503c276c74ca137567f2e3cd836ccb003305c0
Vanshaj-kaushal <vanshajkaushal1.0@gmail.com>
2023-06-28 15:27:50 +0530

    added image link file

--- src/Header/comps/Interface.tsx
+++ src/Header/comps/Interface.tsx
@@ -2,6 +2,13 @@ export interface links {
   links: link[]
 }
 
+export interface imageLink {
+  class: string,
+  href: string,
+  src: string,
+  alt: string
+}
+
 export interface link {
   class: string,
   href: string,
