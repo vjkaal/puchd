98f952acba30328cffc69d65a675bd6f8ebb84fc
Vanshaj-kaushal <vanshajkaushal1.0@gmail.com>
2023-06-28 15:52:53 +0530

    added logo image

--- src/Header/Logo.tsx
+++ src/Header/Logo.tsx
@@ -1,8 +1,15 @@
+import logo from './../res/logo/panjab-university.svg'
+
 export function Logo() {
   return(
     <div className="logo">
-      <h1>PANJAB UNIVERSITY</h1>
-      <h4>Established in 1882</h4>
+      <div className="image">
+        <img src={logo} alt="Panjab University" width="200px" />
+      </div>
+      <div className="text">
+        <h1>PANJAB UNIVERSITY</h1>
+        <h4>Established in 1882</h4>
+      </div>
     </div>
   )
 }
