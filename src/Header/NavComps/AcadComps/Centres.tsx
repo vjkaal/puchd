c725fbd00bb122a1c9710993b4cba6b6d881053d
Vanshaj-kaushal <vanshajkaushal1.0@gmail.com>
2023-06-30 15:45:04 +0530

    added nav>acad>centres

--- src/Header/NavComps/AcadComps/Centres.tsx
+++ src/Header/NavComps/AcadComps/Centres.tsx
@@ -1,3 +1,52 @@
+import {Links} from "../../comps/Links";
+
 export function Centres() {
-  
+  const centres = [
+    {
+      class: "arts",
+      href: "#",
+      text: "arts"
+    },
+    {
+      class: "social",
+      href: "#",
+      text: "social science"
+    },
+    {
+      class: "language",
+      href: "#",
+      text: "languages"
+    },
+    {
+      class: "science",
+      href: "#",
+      text: "science"
+    },
+    {
+      class: "medical",
+      href: "#",
+      text: "medical sciences"
+    },
+    {
+      class: "regional",
+      href: "#",
+      text: "regional centres"
+    },
+    {
+      class: "rural",
+      href: "#",
+      text: "rural centres"
+    },
+    {
+      class: "study",
+      href: "#",
+      text: "study centres"
+    }
+  ]
+
+  return (
+    <div className="centres-list">
+      <Links links={centres} />
+    </div>
+  )
 }
