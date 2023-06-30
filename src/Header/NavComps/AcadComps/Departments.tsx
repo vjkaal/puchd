7a41b0e11caccecf7d514acae86b04759557a865
Vanshaj-kaushal <vanshajkaushal1.0@gmail.com>
2023-06-30 15:21:10 +0530

    added nav>acad>depts

--- src/Header/NavComps/AcadComps/Departments.tsx
+++ src/Header/NavComps/AcadComps/Departments.tsx
@@ -1,3 +1,46 @@
+import {Links} from '../../comps/Links'
+
 export function Departments() {
-  
+  const depts = [
+    {
+      class: "arts",
+      href: "#",
+      text: "arts"
+    },
+    {
+      class: "design",
+      href: "#",
+      text: "design and fine arts"
+    },
+    {
+      class: "education",
+      href: "#",
+      text: "education"
+    },
+    {
+      class: "language",
+      href: "#",
+      text: "language"
+    },
+    {
+      class: "law",
+      href: "#",
+      text: "law"
+    },
+    {
+      class: "science",
+      href: "#",
+      text: "science"
+    },
+    {
+      class: 'multi-fac',
+      href: '#',
+      text: 'multi-facility departments'
+    }
+  ]
+  return(
+    <div className="dept-links-list">
+      <Links links={depts} />
+    </div>
+  )
 }
