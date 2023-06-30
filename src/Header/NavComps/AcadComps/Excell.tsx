ee4d3513c681555dd1efeba3e236b2df5109af99
Vanshaj-kaushal <vanshajkaushal1.0@gmail.com>
2023-06-30 16:55:57 +0530

    added nav>acad>excell

--- src/Header/NavComps/AcadComps/Excell.tsx
+++ src/Header/NavComps/AcadComps/Excell.tsx
@@ -1,3 +1,32 @@
+import {Links} from "../../comps/Links";
+
 export function Excell() {
-  
+  const excel = [
+    {
+      class: "cas",
+      href: "#",
+      text: "Centres for Advanced Study (CAS)"
+    },
+    {
+      class: "drs",
+      href: "#",
+      text: "Departmental Research Support (DRS)"
+    },
+    {
+      class: "dsa",
+      href: "#",
+      text: "Department of Special Assistance (DSA)"
+    },
+    {
+      class: "fist",
+      href: "#",
+      text: "Funds for Improvement Of Science and Technology (FlST)"
+    }
+  ]
+
+  return (
+    <div className="excell-list">
+      <Links links={excel} />
+    </div>
+  )
 }
